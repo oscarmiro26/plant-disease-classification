ee68609be0cb3186cb97c0ab6819da375f2d6e7e
Oscar Miro Lopez Feliu O.Miro.I.Lopez.Feliu@student.rug.nl <oscarmiro26@hotmail.com>
2025-05-13 14:16:21 +0200

    Created src/training/losses.py - for now contains FocalLoss, which is very similar to weighted cross entropy loss but focuses more on minority classes.

---
+++ src/training/losses.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FocalLoss(nn.Module):
+    """
+    Focal loss for multi-class classification.
+    See https://arxiv.org/abs/1708.02002.
+    """
+    def __init__(self, alpha=None, gamma: float = 2.0, reduction: str = "mean"):
+        super().__init__()
+        if alpha is not None:
+            self.alpha = torch.tensor(alpha, dtype=torch.float32)
+        else:
+            self.alpha = None
+        
+        self.gamma = gamma
+        self.reduction = reduction
+        self.eps = 1e-8
+
+    def forward(self, logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+        if self.alpha is not None:
+            self.alpha = self.alpha.to(logits.device)
+
+        ce_loss = F.cross_entropy(
+            logits, 
+            labels, 
+            weight=self.alpha,
+            reduction="none"
+        )
+
+        pt = torch.exp(-ce_loss)
+        focal = ((1 - pt) ** self.gamma) * ce_loss
+
+        if self.reduction == "mean":
+            focal = focal.mean()
+        elif self.reduction == "sum":
+            focal = focal.sum()
+        return focal
