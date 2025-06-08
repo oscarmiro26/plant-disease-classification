415357b61f99318fe3d19ef40bb0c0be369fd1cd
Richard <>
2025-06-08 21:03:42 +0200

    fix bad import

--- src/postprocessing/uncertainty_calibration.py
+++ src/postprocessing/uncertainty_calibration.py
@@ -164,8 +164,8 @@ def main(args):
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=config.NORMALIZE_MEAN,
-                             std=config.NORMALIZE_STD),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
     ])
     test_transform = val_transform  # same for calibration & evaluation
 
